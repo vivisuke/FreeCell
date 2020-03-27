ad539e47e2d8740536eb4f8450739034e44e876b
vivisuke <ntsuda@master.email.ne.jp>
2020-03-27 11:42:10 +0900

    ◎ C 03/27 03/27 0.001 ToDo MainWindow サイズを固定に

--- FreeCell/QtFreeCell/MainWindow.cpp
+++ FreeCell/QtFreeCell/MainWindow.cpp
@@ -5,6 +5,7 @@ MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	setFixedSize(600, 600*1.6);
 	auto fc = new FreeCellWidget();
 	setCentralWidget(fc);
 	setWindowTitle("QtFreeCell");
