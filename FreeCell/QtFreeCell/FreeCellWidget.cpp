762a02d28aa12d68ef6bb8fdaa3a6847ee29a38b
vivisuke <ntsuda@master.email.ne.jp>
2020-03-27 09:24:28 +0900

    ◎ C 03/27 03/27 0.001 ToDo フリーセル・ホームセル枠表示 ◎ C 03/27 03/27 0.001 ToDo ホームセル："A" 表示

--- FreeCell/QtFreeCell/FreeCellWidget.cpp
+++ FreeCell/QtFreeCell/FreeCellWidget.cpp
@@ -1,4 +1,5 @@
 ﻿#include <QPainter>
+#include "../FreeCell/Board.h"
 #include "FreeCellWidget.h"
 
 FreeCellWidget::FreeCellWidget(QWidget *parent)
@@ -10,16 +11,39 @@ FreeCellWidget::FreeCellWidget(QWidget *parent)
 }
 void FreeCellWidget::paintEvent(QPaintEvent*event)
 {
-	auto rct = rect();
 	QPainter pt(this);
+	//
+	auto rct = rect();
+	auto cdWidth = rct.width() / 8;
+	auto cdHeight = cdWidth * 90 / 60;
+	//
 #if	0
 	pt.setPen(Qt::black);
 	pt.drawLine(0, 0, rct.width(), rct.height());
 	pt.drawLine(rct.width(), 0, 0, rct.height());
 #endif
-	pt.setBrush(QColor("darkgreen"));
+	pt.setBrush(QColor("darkgreen"));		//	背景色
 	pt.drawRect(rct);
-	auto rct2 = QRect(100, 100, m_rctCard.width()/8, m_rctCard.height() / 7);
+	//	フリーセル・ホームセル枠
+	qreal px = 0;
+	qreal py = 0;
+	pt.setPen(Qt::gray);
+	pt.setBrush(Qt::transparent);
+	for (int i = 0; i != N_FREECELL+N_HOME; ++i, px += cdWidth) {
+		auto r = QRect(px, py, cdWidth, cdHeight);
+		pt.drawRoundedRect(r, cdWidth/10, cdWidth/10);
+	}
+	//	ホームセル内に「A」表示
+	pt.setFont(QFont("Arial", 40));
+	px = cdWidth * N_FREECELL;
+	for (int i = 0; i != N_HOME; ++i, px += cdWidth) {
+		auto r = QRect(px, py, cdWidth, cdHeight);
+		pt.drawText(r, Qt::AlignCenter|Qt::AlignVCenter, "A");
+	}
+	//
+	px = 0;
+	py = cdHeight + cdWidth/2;
+	auto rct2 = QRect(px, py, cdWidth, cdHeight);
 	//rct.setHeight(rct.width() * m_rctCard.height() / m_rctCard.width());
 	pt.drawImage(rct2, m_imgCard, m_rctCard1);
 }
